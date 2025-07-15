0a893b2d9070d47e2637b2f2fd961a1e62d6b0dc
unknown <vahidsayfollahzadeh@gmail.com>
2025-07-15 15:10:54 +0330

    fix: router base url

--- src/index.js
+++ src/index.js
@@ -40,7 +40,7 @@ root.render(
  <GoogleOAuthProvider clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}>
   <Provider store={store}>
    <PersistGate loading={null} persistor={persistor}>
-    <BrowserRouter>
+    <BrowserRouter basename='/'>
      <PersistQueryClientProvider
       client={queryClient}
       persistOptions={{ persister }}>
